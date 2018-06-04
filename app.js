40531a008e6574072542a9d0a5e032f1df43ed09
dxh <xiao00hei@163.com>
2018-06-04 13:50:48 +0800

    dxh

--- app.js
+++ app.js
@@ -19,7 +19,34 @@ app.use(bodyPreser.json());
 var MongoClient = require('mongodb').MongoClient;
 var mongoUrl = "mongodb://127.0.0.1:27017/dxh";
 
-//
+//保存session
+const session = require("express-session");
+//配置中间件
+app.use(session({
+    secret: 'chenglv',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge:1000*60*30  //30分钟
+    }
+    ,rolling:true
+}));
+
+//中间件session过滤
+app.use((req,res,next)=>{
+    let _url_ = req.url;
+    if(_url_=="/" || _url_=="/login" || _url_=="/doLogin"){
+        next();
+    }else{
+        if(session.userInfo && session.userInfo.username!=""){
+            next();
+        }else{
+            res.redirect("/login");
+        }
+    }
+})
+
+//首页
 app.get("/",(req,res)=>{
    // res.send("shouyee")
     res.redirect("/login");
@@ -29,6 +56,7 @@ app.get("/",(req,res)=>{
 app.get("/login",(req,res)=>{
     res.render("auth/login")
 });
+//登陆操作
 app.post("/doLogin",(req,res)=>{
 
     MongoClient.connect(mongoUrl, (err, client)=>{
@@ -41,6 +69,7 @@ app.post("/doLogin",(req,res)=>{
         cursor.toArray((err,data)=>{
             if(data.length>0){
                 console.log("登录成功"); //
+                session.userInfo = data[0];
                 res.redirect("/productList");
             }else{
                 console.log("登录失败");
